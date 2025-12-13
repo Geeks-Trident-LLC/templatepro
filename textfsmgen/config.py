6f185c7c3a8f43184cfc4c33eb755e5d644e7c82
Tuyen M. Duong <tuyen@geekstrident.com>
2025-12-12 21:57:47 -0800

    chore: bump version from 0.2.1a1 to 0.2.1a2

--- textfsmgen/config.py
+++ textfsmgen/config.py
@@ -12,7 +12,7 @@ import yaml
 
 from genericlib import version as gtlib_version
 
-__version__ = '0.2.1a1'
+__version__ = '0.2.1a2'
 version = __version__
 __edition__ = 'Community'
 edition = __edition__
