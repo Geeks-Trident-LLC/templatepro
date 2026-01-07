5faab6e1444ed58ebc6eb7368de95f1f1932938d
Tuyen Mathew Duong <tuyen@geekstrident.com>
2026-01-07 03:38:01 -0800

    refactor(gpdiff): simplify module definitions, utilities, and unit tests

--- tests/unit/gpcommon/__init__.py
+++ tests/unit/gpcommon/__init__.py
@@ -4,7 +4,7 @@ Unit tests for the `textfsmgen.gpcommon` module.
 Usage
 -----
 Run pytest in the project root to execute these tests:
-    $ pytest tests/unit/gpcategory
+    $ pytest tests/unit/gpcommon
     or
     $ python -m pytest tests/unit/gpcommon
 """
---
+++ tests/unit/gpdiff/__init__.py
@@ -0,0 +1,10 @@
+"""
+Unit tests for the `textfsmgen.gpdiff` module.
+
+Usage
+-----
+Run pytest in the project root to execute these tests:
+    $ pytest tests/unit/gpdiff
+    or
+    $ python -m pytest tests/unit/gpdiff
+"""
